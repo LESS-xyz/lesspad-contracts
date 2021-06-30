5d395839f5182e01f5205f7da13cfccb5db17fbf
Rita Tcepeleva <34192171+RitaTcepeleva@users.noreply.github.com>
2021-06-30 17:54:13 +0300

    Add files via upload

---
+++ migrations/3_library_migrations.js
@@ -0,0 +1,5 @@
+const LessLibrary = artifacts.require("LessLibrary");
+
+module.exports = function (deployer) {
+  deployer.deploy(LessLibrary, "0x6619D419A27bb005a0a51DA2610d036133E72a72", "0x8be1038D11c19F86071363E818A890014cBf3433");
+};
